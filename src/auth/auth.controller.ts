c1947c149014d1f93c1e7d61ca334bd49aab6e1f
Cannie511 <Canh51102@gmail.com>
2024-09-05 21:17:50 +0700

    nodemailer with nestjs

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,16 @@
-import { Body, Controller, Post, Request, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './passport/local-auth.guard';
 import { Public } from 'src/decorator/customize';
 import { CreateAuthDto } from './dto/create-auth.dto';
+import { MailerService } from '@nestjs-modules/mailer';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly mailService: MailerService
+  ) {}
   @Public()
   @UseGuards(LocalAuthGuard)
   @Post('login')
@@ -18,5 +22,20 @@ export class AuthController {
   @Post('register')
   register(@Body() registerDto: CreateAuthDto) {
     return this.authService.handleRegister(registerDto);
+  }
+
+  @Public()
+  @Get('mail')
+  testMail() {
+    this.mailService
+      .sendMail({
+        to: 'Canh51102@gmail.com', // list of receivers
+        subject: 'Testing Nest MailerModule ✔', // Subject line
+        text: '[Hello World]', // plaintext body
+        html: '<b>Hello Word with Nest</b>', // HTML body content
+      })
+      .then(() => {})
+      .catch(() => {});
+    return 'ok';
   }
 }
